2afcad31f213ba3e5391ebe1f7411ea60db7f0a3
abubakarafzal0101 <abubakarafzal0101@gmail.com>
2026-05-15 16:42:56 +0500

    site first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="text-4xl font-bold">
+      Vivant Skin CLinic will be Live Soon!
+    </div>
+  );
+};
+
+export default App;
